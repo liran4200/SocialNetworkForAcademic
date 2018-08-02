5e2d1d7c61cee125ae353104be29fcc43616d98a
nirfinz <nirfinz2@gmail.com>
2018-08-02 12:39:52 +0300

    updated profile and edit profile

---
+++ app/src/main/java/com/example/liranyehudar/socialnetworkforacademic/logic/ParentCategory.java
@@ -0,0 +1,18 @@
+package com.example.liranyehudar.socialnetworkforacademic.logic;
+
+public class ParentCategory {
+
+    private String name;
+
+    public ParentCategory(String name){
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
