3a5926383b37597a6e272265ce66a7a806f80952
liran4200 <liran4200@gmail.com>
2018-08-03 17:46:28 +0300

    Add feed posts UI

--- app/src/main/java/com/example/liranyehudar/socialnetworkforacademic/fragments/HomeFeedFragment.java
+++ app/src/main/java/com/example/liranyehudar/socialnetworkforacademic/fragments/HomeFeedFragment.java
@@ -3,14 +3,24 @@ package com.example.liranyehudar.socialnetworkforacademic.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.example.liranyehudar.socialnetworkforacademic.R;
+import com.example.liranyehudar.socialnetworkforacademic.activities.ProfileActivity;
 import com.example.liranyehudar.socialnetworkforacademic.activities.ProfileEditActivity;
+import com.example.liranyehudar.socialnetworkforacademic.logic.ModelFeed;
+import com.example.liranyehudar.socialnetworkforacademic.logic.RecycleViewAdpaterFeeds;
 import com.example.liranyehudar.socialnetworkforacademic.logic.Student;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -19,21 +29,29 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class HomeFeedFragment extends Fragment {
 
-    Student student;
     CircleImageView profile;
+    TextView txtPost;
+    RecyclerView recyclerView;
+    RecycleViewAdpaterFeeds adpaterFeeds;
+    ArrayList<ModelFeed> modelFeedArrayList;
 
     public HomeFeedFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view  = inflater.inflate(R.layout.fragment_home_feed, container, false);
-        //student = (Student) getArguments().getSerializable("student");
-        profile = view.findViewById(R.id.profile_image);
+
+        bindUI(view);
+        modelFeedArrayList = getData();
+
+        adpaterFeeds = new RecycleViewAdpaterFeeds(modelFeedArrayList,view.getContext());
+        recyclerView.setAdapter(adpaterFeeds);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +60,37 @@ public class HomeFeedFragment extends Fragment {
             }
         });
 
+        txtPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return view;
+    }
+
+    private ArrayList<ModelFeed> getData() {
+        ArrayList<ModelFeed> list = new ArrayList<>();
+        list.add(new ModelFeed(1,22,33,"Liran Yehudar",
+                "Im Looking for my next challenger in web developer, please subscribe me","5 mins"));
+
+
+        list.add(new ModelFeed(2,10,1,"NirFinz",
+                "Im Kirchleh ya movrachhh, eize yuri ze ","8 mins"));
+
+        list.add(new ModelFeed(3,13,5,"Yuri Vainshtein",
+                "There is IOS DEVELOPER ???? , I need a help with my storage application.","1 hour"));
+
+        list.add(new ModelFeed(4,100,99,"Dudu Kirchly",
+                "Ma ze ze Tuna??, Tuna Tuna , Mborach mobrach bo lepo","13 hours"));
+        return list;
+    }
+
+    private void bindUI(View view) {
+        profile = view.findViewById(R.id.profile_image);
+        recyclerView = view.findViewById(R.id.recycleView_posts);
+        txtPost = view.findViewById(R.id.txt_post);
     }
 
 }
---
+++ app/src/main/java/com/example/liranyehudar/socialnetworkforacademic/logic/RecycleViewAdpaterFeeds.java
@@ -0,0 +1,66 @@
+package com.example.liranyehudar.socialnetworkforacademic.logic;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.example.liranyehudar.socialnetworkforacademic.R;
+
+import java.util.ArrayList;
+
+public class RecycleViewAdpaterFeeds extends RecyclerView.Adapter<RecycleViewAdpaterFeeds.ModelFeedViewHolder>{
+
+    ArrayList<ModelFeed> modelFeedArrayList;
+    Context context;
+
+    public RecycleViewAdpaterFeeds(ArrayList<ModelFeed> modelFeedArrayList, Context context) {
+        this.modelFeedArrayList = modelFeedArrayList;
+        this.context = context;
+    }
+
+    @Override
+    public ModelFeedViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem_posts,parent,false);
+        ModelFeedViewHolder modelFeedViewHolder = new ModelFeedViewHolder(view);
+        return modelFeedViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder( ModelFeedViewHolder holder, int position) {
+
+        holder.fullName.setText(modelFeedArrayList.get(position).getFullName());
+        holder.txtStatus.setText(modelFeedArrayList.get(position).getStatus());
+        holder.txtTime.setText(modelFeedArrayList.get(position).getTime());
+        holder.txtCommentsAmount.setText(modelFeedArrayList.get(position).getAmountOfComments()+"");
+        holder.txtLikesAmount.setText(modelFeedArrayList.get(position).getAmountOfLikes()+"");
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelFeedArrayList.size();
+    }
+
+    public class ModelFeedViewHolder extends RecyclerView.ViewHolder {
+
+        TextView fullName;
+        TextView txtLikesAmount;
+        TextView txtCommentsAmount;
+        TextView txtTime;
+        TextView txtStatus;
+
+        public ModelFeedViewHolder(View itemView) {
+            super(itemView);
+
+            fullName = itemView.findViewById(R.id.txt_fullname);
+            txtLikesAmount =itemView.findViewById(R.id.txt_likes_amount);
+            txtCommentsAmount = itemView.findViewById(R.id.txt_comment);
+            txtTime = itemView.findViewById(R.id.txt_date_time);
+            txtStatus = itemView.findViewById(R.id.txt_status);
+
+        }
+    }
+}
