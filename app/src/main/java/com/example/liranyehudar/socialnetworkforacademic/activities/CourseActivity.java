5f0623c711a3eb9f14edbc5701494369264efccc
liran4200 <liran4200@gmail.com>
2018-08-08 13:16:21 +0300

    Add firebase configuration

--- app/src/main/java/com/example/liranyehudar/socialnetworkforacademic/activities/CourseActivity.java
+++ app/src/main/java/com/example/liranyehudar/socialnetworkforacademic/activities/CourseActivity.java
@@ -2,6 +2,7 @@ package com.example.liranyehudar.socialnetworkforacademic.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.liranyehudar.socialnetworkforacademic.R;
@@ -21,6 +22,7 @@ public class CourseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
         init();
         setUI();
     }
@@ -39,13 +41,12 @@ public class CourseActivity extends AppCompatActivity {
                "Mr.Adi Malach",Course.Semester.FIRST,
                Course.Day.MONDAY,
                new Time("10","32"),new Time("13","00"));
-
-       courseName.setText(course.getCourseName());
-       courseNumnber.setText(course.getCourseNumber());
-       semester.setText(course.getSemester().name());
-       lecturer.setText(course.getLecture());
-       day.setText(course.getDay().name());
-       time.setText(course.getStartTime() + "-" +course.getEndTime());
+        courseName.setText(course.getCourseName());
+        courseNumnber.setText(course.getCourseNumber()+"");
+        semester.setText(course.getSemester().name());
+        lecturer.setText(course.getLecture());
+        day.setText(course.getDay().name());
+        time.setText(course.getStartTime() + "-" + course.getEndTime());
     }
 
 
